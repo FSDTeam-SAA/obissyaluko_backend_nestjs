a83df1f7c478f555f03150ead53a8b770e5ccfbb
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-05-30 12:49:38 +0600

    feat: implement consultation and FAQ modules with full CRUD functionality and schema definitions

---
+++ src/app/module/faq/entities/faq.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type FaqDocument = HydratedDocument<Faq>;
+
+@Schema({ timestamps: true })
+export class Faq {
+  @Prop()
+  question: string;
+
+  @Prop()
+  answer: string;
+
+  @Prop()
+  category?: string;
+}
+
+export const FaqSchema = SchemaFactory.createForClass(Faq);
