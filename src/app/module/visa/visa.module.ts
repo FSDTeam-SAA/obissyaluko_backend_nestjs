017d31d1d0c0c5260882aa596ac7b069ee05ce78
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-06-09 08:27:12 +0600

    feat: implement and register VisaModule in the application module

--- src/app/module/visa/visa.module.ts
+++ src/app/module/visa/visa.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { VisaService } from './visa.service';
 import { VisaController } from './visa.controller';
@@ -17,19 +16,3 @@ import { Country, CountrySchema } from '../country/entities/country.entity';
   providers: [VisaService],
 })
 export class VisaModule {}
-=======
-import { Module } from '@nestjs/common';
-import { VisaService } from './visa.service';
-import { VisaController } from './visa.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Visa, VisaSchema } from './entities/visa.entity';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Visa.name, schema: VisaSchema }]),
-  ],
-  controllers: [VisaController],
-  providers: [VisaService],
-})
-export class VisaModule {}
->>>>>>> daca00e8f27707a1630862b2681a5c74acfa355a
